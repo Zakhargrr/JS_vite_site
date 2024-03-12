918bd758ed8cb1a709c0e75e33ea54fe76748e2b
EskovaAl <131173384+EskovaAl@users.noreply.github.com>
2024-03-12 15:00:56 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import my_button from './components/my_button'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Content from './components/Content'
+import LabList from './components/LabsList'
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <Header>
-        <h1>Список лабораторных работ</h1>
-      </Header>
-      
-      <div className="card">
-        <my_button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </my_button>
-        <my_button onClick={() => alert("Hello world")}>
-          Выводим alert
-        </my_button>
-        <p>
-          Это работающие кнопки
-        </p>
+      <Header />
+
+      <div className="main_page">
+        <LabList />
+        <Content />
       </div>
-      <div className="labsList">
-        <h2>Это список лабораторных работ</h2>
-        
-      </div>
-      <Footer>
-        <h2>Еськова А.А.</h2>
-      </Footer>
+
+      <Footer />
+
     </>
   )
 }
