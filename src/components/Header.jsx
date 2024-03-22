e642ed9063088b9794be4ee3bb21b5461f36f85c
Alina <131173384+EskovaAl@users.noreply.github.com>
2024-03-22 04:07:17 +0000

    dd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
-import React from "react"
-import './comp.css'
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
-import StarIcon from '@mui/icons-material/Star';
+import React from "react";
+import "./comp.css";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import { SportsBasketball } from "@mui/icons-material";
 
 const Header = () => {
-    return (
-        <div className="header_main">
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography fontSize={36} flexGrow={2} fontFamily={"Cambria Math"}>
-                        Мой Гениальный сайт
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-
-    )
-}
-export default Header
+  return (
+    <div className="header_main">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography fontSize={36} flexGrow={2} fontFamily={"Cambria Math"}>
+            Мой Гениальный сайт
+          </Typography>
+          <IconButton>
+            <StarIcon></StarIcon>
+          </IconButton>
+          <IconButton>
+            <SportsBasketball />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+export default Header;
